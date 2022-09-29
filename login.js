e398aa3e6797a1c267c5034b65d06ca7af7b8a90
Tushar Raihan <sheikhhasinur69@gmail.com>
2022-09-30 02:54:56 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById('button-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+
+
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href ='bank.html';
+    } else {
+        alert('Invalid User')
+    }
+});
+
